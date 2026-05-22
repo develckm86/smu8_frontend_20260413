b3988c910680e492217fcdb4c62e9bc1c19801da
develckm86 <develckm86@gmail.com>
2026-05-22 11:42:12 +0900

    mpa 부서 crud

--- expressjs_oracledb/ex3_dept_router.js
+++ expressjs_oracledb/ex3_dept_router.js
@@ -4,28 +4,86 @@ import oracledb from "oracledb";
 const router=express.Router();
 router.use(express.urlencoded({extended:true}));
 
-router.post("/modify",(req,res,next)=>{
+router.get("/:deptno/remove",async(req, res)=>{
+    let deptno=req.params.deptno;
+    let sql=`DELETE FROM DEPT WHERE DEPTNO=:deptno`;
+    let conn;
+    try {
+        conn=await oracledb.getConnection();
+        const result=await conn.execute(sql,[deptno]);
+        console.log(result);
+        conn.commit();
+        res.redirect("/dept");
+    }catch (e) {
+        throw e;
+    }finally {
+        conn && await conn.close()
+    }
+});
+
+
+router.get("/regist",(req,res)=>{
+   res.render("deptRegist");
+});
+router.post("/regist",async(req,res)=>{
+    req.body.deptno=Number(req.body.deptno);
     console.log(req.body);
-    res.send("수정")
+    let sql=`INSERT INTO DEPT (DEPTNO, DNAME, LOC) 
+                VALUES (:deptno,:dname,:loc)`;
+    let conn;
+    try {
+        conn=await oracledb.getConnection();
+        const result=await conn.execute(sql,req.body);
+        console.log(result);
+        conn.commit();
+        res.redirect("/dept");
+    }catch (e) {
+        throw e;
+    }finally {
+        conn && await conn.close();
+    }
+});
+
+
+router.post("/modify",async (req,res,next)=>{
+    //let deptno=req.body.deptno;
+    let {deptno,dname,loc}=req.body;
+    deptno=Number(deptno);
+    //해당 부서가 없거나, 부서번호가 없거나, 부서이름과 위치 유효성검사
+    let sql=`UPDATE DEPT SET DNAME=:dname, LOC=:loc WHERE DEPTNO=:deptno`;
+    let conn;
+    try {
+        conn=await oracledb.getConnection();
+        const result=await conn.execute(sql,{
+            dname : dname,
+            loc:loc,
+            deptno:deptno
+        });
+        conn.commit();
+        console.log(result);
+        res.redirect("/dept")
+    }catch (e) {
+        throw e;
+    }finally {
+        conn?.close();
+    }
 });
 
 //app.use("/dept",deptRouter);
 router.get("/",async (req, res, next)=>{
     let conn;
     try {
+        let sql=`SELECT * FROM DEPT ORDER BY DEPTNO DESC`;
         conn=await oracledb.getConnection();
-        const result=await conn.execute("select * from dept");
+        const result=await conn.execute(sql);
         //res.json(JSON.stringify(result.rows))
-        res.render("deptList.pug",{depts:result.rows})
+        res.render("deptList",{depts:result.rows})
     }catch (e) {
         next(e);//throw e; 오류처리하는 미들웨어 호출
     }finally {
         conn?.close();
     }
 });
-
-
-
 
 router.get("/:deptno",async (req,res,next)=>{
     let deptno=req.params.deptno;
